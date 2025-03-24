67affd99111996bea71cf40cabfeb36a711eb840
tanimsk <sktanim5800@gmail.com>
2025-03-24 13:01:26 +0600

    Database Modification

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS todos;
+
+CREATE TABLE
+  users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP
+    WITH
+      TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  );
+
+CREATE TABLE
+  todos (
+    id SERIAL PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    is_completed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP
+    WITH
+      TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  );
+
+
+-- Indexes
+CREATE INDEX idx_users_email ON users (email);
+CREATE INDEX idx_todos_user_id ON todos (user_id);
