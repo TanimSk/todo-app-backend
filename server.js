1a29b7619dbb05c6108999430e88d2c08c91f033
tanimsk <sktanim5800@gmail.com>
2025-04-11 19:25:42 +0600

    added api docs

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 require("dotenv").config();
 
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+
 const todoRoutes = require("./routes/todoRoutes");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+const swaggerDocument = YAML.load("./openapi.yaml");
 const PORT = process.env.PORT || 3000;
 
 // Middleware
@@ -15,6 +19,8 @@ app.use(morgan("dev"));
 // Routes
 app.use("/api/todos", todoRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 // Root route
 app.get("/", (req, res) => {
